c36efbc05ac05d84c9c8341bbd168e2c5b68fdd1
James Zhang <jameszhanga@gmail.com>
2023-02-17 01:29:44 -0500

    remove examples

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { habitRouter } from "./routers/habit";
 
 /**
@@ -8,7 +7,6 @@ import { habitRouter } from "./routers/habit";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   habit: habitRouter,
 });
 
