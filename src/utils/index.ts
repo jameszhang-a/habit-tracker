f568a83e4e08cd969a154153f05d08207e59fbed
James Zhang <jameszhanga@gmail.com>
2023-03-17 22:05:35 -0700

    function to get week num from date

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const formatDate = (date: Date) => {
+const formatDate = (date: Date) => {
   return date.toLocaleString("en-GB", {
     day: "numeric",
     month: "short",
@@ -8,8 +8,30 @@ export const formatDate = (date: Date) => {
   });
 };
 
-export const emojiLength = (text: string) => {
+const emojiLength = (text: string) => {
   const x = [...new Intl.Segmenter().segment(text)];
 
   return x.length;
 };
+
+const weekFromDate = (date: Date) => {
+  const time = date.getTime();
+
+  // Create a new Date object for the beginning of the year
+  const startOfYear = new Date(time);
+  startOfYear.setMonth(0, 1);
+  startOfYear.setHours(0, 0, 0, 0);
+
+  // Calculate the difference between the input date and the beginning of the year in milliseconds
+  const diff = time - startOfYear.getTime();
+
+  // Convert the difference to days and add 1 to get the day number out of the year (starting at 1)
+  const dayOfYear = Math.floor(diff / (1000 * 60 * 60 * 24)) + 1;
+
+  const dayOfYearOffset = startOfYear.getDay() - 1;
+
+  return Math.ceil((dayOfYear + dayOfYearOffset) / 7);
+};
+console.log(weekFromDate(new Date()));
+
+export { formatDate, emojiLength, weekFromDate };
