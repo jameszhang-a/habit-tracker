ab383ca064860ce5c85d1c649a7ab361cb617075
James Zhang <jameszhanga@gmail.com>
2023-09-24 15:39:26 -0700

    remove package

--- src/components/Refresh.tsx
+++ src/components/Refresh.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { RefreshCircleOutline } from "react-ionicons";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 const RefreshButton = () => {
   const [hover, setHover] = useState(false);
@@ -11,15 +11,15 @@ const RefreshButton = () => {
       onMouseOver={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
       onClick={() => router.reload()}
-      className="absolute bottom-3 right-5 rounded-md text-lg font-bold text-white shadow-lg transition duration-200 ease-in-out hover:shadow-xl"
+      className="absolute bottom-3 right-5 rounded-md text-lg font-bold text-white transition duration-200 ease-in-out"
     >
-      <RefreshCircleOutline
+      <ArrowPathIcon
         color={`${
-          hover ? "rgba(149, 147, 217, 1)" : "rgba(149, 147, 217, 0.1)"
+          hover ? "rgba(37, 39, 46, 0.5)" : "rgba(149, 147, 217, 0.1)"
         }`}
+        style={{ transition: "all .15s ease" }}
         height="25px"
         width="25px"
-        rotate={hover}
       />
     </button>
   );
