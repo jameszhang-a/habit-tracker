c57787f6d3ac19a48575d3fa12f7bbba7d19d7b5
James Zhang <jameszhanga@gmail.com>
2023-10-08 18:36:34 -0700

    Changing spacing

--- src/components/TrackerBackground.tsx
+++ src/components/TrackerBackground.tsx
@@ -4,7 +4,7 @@ interface TrackerBackgroundProps {
 }
 
 const defaultClass =
-  "relative flex flex-col items-center justify-center rounded-2xl border border-gray-200 bg-gradient-to-br px-4 py-4 shadow backdrop-blur max-h-[300px]";
+  "relative flex flex-col items-center rounded-2xl border border-gray-200 bg-gradient-to-br px-4 py-4 shadow backdrop-blur max-h-[240px]";
 
 const TrackerBackground: React.FC<TrackerBackgroundProps> = ({
   theme,
