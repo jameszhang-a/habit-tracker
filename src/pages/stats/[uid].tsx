fb808f644a059dfd89ba8c249f02b37cf580eb97
James Zhang <jameszhanga@gmail.com>
2023-10-15 01:27:06 -0700

    Adding habit tabs

--- src/pages/stats/[uid].tsx
+++ src/pages/stats/[uid].tsx
@@ -1,10 +1,12 @@
 import { type NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/router";
 
 import CompletionChart from "@/components/Charts/CompletionChart";
 import HeadWrapper from "@/components/HeadWrapper";
 import StatCard from "@/components/StatCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { api } from "@/utils/api";
 
 import type { Habits } from "@/types";
@@ -13,6 +15,7 @@ const habitAPI = api.habit;
 
 const Stats: NextPage = () => {
   const [habits, setHabits] = useState<Habits>([]);
+  const [selectedHabit, setSelectedHabit] = useState<string>("");
 
   const router = useRouter();
   const uid = router.query.uid as string;
@@ -24,8 +27,20 @@ const Stats: NextPage = () => {
   useEffect(() => {
     if (habitsData) {
       setHabits(habitsData);
+      setSelectedHabit(habitsData[0]?.id ?? "");
     }
   }, [habitsData]);
+
+  const tabs = useMemo(
+    () =>
+      habitsData &&
+      habitsData.map((habit) => (
+        <TabsTrigger key={habit.id} value={habit.id}>
+          {habit.name}
+        </TabsTrigger>
+      )),
+    [habitsData]
+  );
 
   return (
     <HeadWrapper
@@ -36,6 +51,7 @@ const Stats: NextPage = () => {
         <div>Stats page</div>
         <div>User id: {uid}</div>
         <div className="start-0 flex w-full"></div>
+
         <section
           className={`container relative mb-4 w-[90vw] flex-col items-center gap-4 rounded-xl border border-slate-300 bg-[#f4f5f6]/80 p-5 shadow`}
         >
@@ -43,13 +59,22 @@ const Stats: NextPage = () => {
           <CompletionChart habits={habits} />
         </section>
 
-        <div className="container mx-auto rounded-xl border bg-violet-300">
-          <div className="flex h-[500px] flex-col items-center gap-4 overflow-scroll p-4">
+        <Tabs
+          className="container flex flex-col items-center"
+          // defaultValue={habitsData?.[0]?.id}
+          value={selectedHabit}
+          onValueChange={setSelectedHabit}
+        >
+          <TabsList className="mb-8">{tabs}</TabsList>
+
+          <div className="container mx-auto flex items-center justify-center rounded-xl border bg-violet-300 p-8">
             {habits.map((habit) => (
-              <StatCard key={habit.id} habit={habit} />
+              <TabsContent key={habit.id} value={habit.id}>
+                <StatCard habit={habit} />
+              </TabsContent>
             ))}
           </div>
-        </div>
+        </Tabs>
       </main>
     </HeadWrapper>
   );
