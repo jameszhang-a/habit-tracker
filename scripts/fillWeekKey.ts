f8dcc972dbbe26561e39663f136aeb5d2b00ba0c
James Zhang <jameszhanga@gmail.com>
2024-02-19 11:49:54 +0000

    update script

--- scripts/fillWeekKey.ts
+++ scripts/fillWeekKey.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../src/server/utils/prisma";
-import { weekFromDate } from "../src/utils";
+import { getWeekKey } from "../src/utils";
 
 const calculateWeekKey = async () => {
   const logs = await prisma.habitLog.findMany({
@@ -10,19 +10,17 @@ const calculateWeekKey = async () => {
   });
 
   for (const log of logs) {
-    const logWeek = weekFromDate(log.date);
-    const logYear = log.date.getFullYear();
+    const weekKey = getWeekKey(log.date);
 
     await prisma.habitLog.update({
       where: {
         id: log.id,
       },
       data: {
-        weekKey: `${logYear}-${logWeek}`,
+        weekKey,
       },
     });
   }
 };
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-calculateWeekKey();
+await calculateWeekKey();
